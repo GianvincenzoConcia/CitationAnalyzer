e289e74ec03f9cc06be94b17afdc4ee432d9025d
GianvincenzoConcia / Gianvincenzo Concia <gianvyc99.gc@gmail.com>
2023-11-13 10:58:53 +0100

    commit cerca conferenza

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 from flask import Flask, render_template, request
 import requests
 import re
@@ -10,40 +12,71 @@ from selenium.webdriver.support import expected_conditions as EC
 app = Flask(__name__, template_folder='templates')
 
 
-# La funzione ricerca la conferenza su DBLP
-def cerca_conferenza(conference_query):
-    driver = webdriver.Chrome()  # Assicurati di avere ChromeDriver installato e il path corretto qui
-    driver.get(f"https://dblp.org/search?q={conference_query}")
+# La funzione ricerca la conferenza su DBLP e filtra per anno
+def get_article_titles(conference_title, conference_year):
+    # Utilizza Selenium per navigare su DBLP e ottenere il contenuto della pagina
+    #options = webdriver.ChromeOptions()
+    #options.add_argument('--headless')
+    driver = webdriver.Chrome()
 
     try:
-        # Trova e clicca sul primo risultato della ricerca, dopo aver atteso la presenza di un elemento cliccabile
+        search_url = f"https://dblp.org/search?q={conference_title}"
+        driver.get(search_url)
+
+        # Utilizza Selenium per navigare alla pagina della conferenza e ottenere il contenuto della pagina
         first_result = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'result-list')))
         first_result.click()
-        conference_query = first_result.text.strip()
 
-        # Ottieni il titolo dalla pagina del risultato
-       # WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, 'headline')))
-       # conference_query = driver.find_element(By.CLASS_NAME, 'headline').text.strip()
+        # Ottieni il contenuto della pagina
+        page_content = driver.page_source
 
+        # Utilizza Beautiful Soup per analizzare il contenuto HTML
+        soup = BeautifulSoup(page_content, 'html.parser')
+
+        year_element = soup.find('span', {'itemprop': 'datePublished'}, string=lambda text: str(conference_year) in text)
+
+        if year_element:
+            # Trova l'elemento "contents" all'interno del contesto di year_element
+            contents_button = year_element.find_next('button', {'class': 'toc-link'})
+
+            if contents_button:
+                # Usa Selenium per cliccare sull'elemento 'contents'
+                contents_button.click()
+
+                # Attendere che la pagina successiva si carichi completamente
+                WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//*[@class="title"]')))
+
+                # Ottenere il nuovo HTML dopo il clic
+                new_page_source = driver.page_source
+
+                # Utilizza BeautifulSoup per analizzare il nuovo HTML
+                soup = BeautifulSoup(new_page_source, 'html.parser')
+
+                # Trova i titoli degli articoli utilizzando Beautiful Soup
+                article_titles = [title.text.strip() for title in soup.find_all('span', class_='title')]
+
+                return article_titles
+        else: print(f"Anno della conferenza {conference_year} non trovato.")
+
+    finally:
         driver.quit()
-        return conference_query
 
-    except:
-        driver.quit()
-        return "Errore durante la ricerca della conferenza su DBLP"
-
+@app.route('/')
+def index():
+    return render_template('interfaccia_web.html', result=None)
 
 # Pagina iniziale con il form di ricerca per la conferenza specifica
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/search', methods=['POST'])
+def search():
     if request.method == 'POST':
-        conference_query = request.form['conference_query']
-        results = cerca_conferenza(conference_query)
-        if results:
-            return render_template('results.html', results=results)
-        else:
-            return "Errore nella ricerca della conferenza."
-    return render_template('interfaccia_web.html')
+        conference_title = request.form['conference_title']
+        conference_year = request.form['conference_year']
+
+        # Ottieni i titoli degli articoli utilizzando Selenium e Beautiful Soup
+        article_titles = get_article_titles(conference_title, conference_year)
+
+        return render_template('interfaccia_web.html', result=article_titles)
+
 
 
 def trova_numero_citazioni_su_google_scholar(nome_articolo):
