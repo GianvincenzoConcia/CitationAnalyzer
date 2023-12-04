9ba3690aa7b9ed255e169644afa17b30824034e1
Francesco Russo <francesco.russo1@gmail.com>
2023-12-04 16:40:48 +0100

    authors_count

--- authors_count.py
+++ authors_count.py
@@ -1,10 +1,10 @@
 from bs4 import BeautifulSoup
 
-from ranking_articles import search_conference, get_year_element, get_contents_link, get_block_elements, init_driver
+from ranking_articles import search_conference, get_year_element, get_contents_link, init_driver
 
 
 def get_author_usage(driver, start_year, end_year, conference_title):
-    block_element_list = []
+    author_list = []
 
     page_conference = search_conference(conference_title, driver)
     if page_conference:
@@ -17,32 +17,41 @@ def get_author_usage(driver, start_year, end_year, conference_title):
 
                 if contents_page_source is not None:
                     block_elements = get_block_elements(contents_page_source)
-                    block_element_list.append(block_elements)
-
-        author_list = get_authors(block_element_list)
+                    for block_element in block_elements:
+                        authors = block_element.find_all("span", attrs={"itemprop": "author"})
+                        authors = [author.find("span", attrs={"itemprop": "name"}).text.strip() for author in authors]
+                        author_list.append(authors)
         print(count_authors(author_list))
 
 
+def get_block_elements(contents_page_source):
+    soup = BeautifulSoup(contents_page_source, "html.parser")
+    blocks = soup.find_all("cite", attrs={"class": "data tts-content"})
+    blocks = [blocks[i] for i in range(1, len(blocks))]
+    return blocks
+
+
 def get_authors(block_elements_list):
-    authors_list = []
+    author_list = []
 
-    for block_elements in block_elements_list:
-        for i in range(1, len(block_elements)):
-            print(block_elements[i])
-            # Itera sugli elementi invece di chiamare find_all
-            for j in block_elements[i].find_all("span", attrs={"itemprop": "author"}):
-
-                author_article = [author.find("span", attrs={"itemprop": "name"}).text.strip() for author in j]
-                for author in author_article:
-                    authors_list.append(author)
-            #print(authors_list)
-    return authors_list
+    for block_element in block_elements_list:
+        authors = block_element.find_all("span", attrs={"itemprop": "author"})
+        authors = [author.find("span", attrs={"itemprop": "name"}).text.strip() for author in authors]
+        author_list.append(authors)
+    print(author_list)
+    return author_list
 
 
 def count_authors(author_list):
-    author_occurrences = {author: author_list.count(author) for author in set(author_list)}
+    authors = []
+    for author in author_list:
+        for a in author:
+            authors.append(a)
+    author_occurrences = {author: authors.count(author) for author in set(authors)}
 
-    return author_occurrences
+    sorted_authors = sorted(author_occurrences.items(), key=lambda x: x[1], reverse=True)
+
+    return sorted_authors
 
 
 get_author_usage(init_driver(), 2012, 2012, "3D Data Processing Visualization and Transmission")
