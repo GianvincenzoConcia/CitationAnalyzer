2b112bba3811c2414ebf1cc627d9e3e6d5151c6b
GianvincenzoConcia / Gianvincenzo Concia <gianvyc99.gc@gmail.com>
2023-12-06 18:52:54 +0100

    Cucumber testing of ranking_articles

--- features/ranking_articles_test.feature
+++ features/ranking_articles_test.feature
@@ -1,7 +1,7 @@
 # Created by gianvincenzoconcia at 06/12/23
 Feature: Test della feature ranking_articles
 
-  Scenario: Ricerca e visualizzazione della classifica
+  Scenario: Ricerca conferenza e visualizzazione della classifica
   Given l'utente è sulla pagina principale
     When l'utente seleziona l'opzione "Classifica articoli più citati"
     And l'utente inserisce "3D Data Processing Visualization and Transmission" come titolo della conferenza
@@ -10,3 +10,27 @@ Feature: Test della feature ranking_articles
     And l'utente clicca sul menù "Seleziona Classifica"
   Then l'utente visualizza la classifica degli articoli
 
+Scenario: Ricerca conferenza e visualizzazione del grafico
+  Given l'utente è sulla pagina principale
+    When l'utente seleziona l'opzione "Classifica articoli più citati"
+    And l'utente inserisce "3D Data Processing Visualization and Transmission" come titolo della conferenza
+  And l'utente inserisce "2012" come anno
+  And l'utente clicca sul pulsante "Cerca"
+    And l'utente clicca sul menù "Seleziona Grafico"
+  Then l'utente visualizza il grafico
+
+  Scenario: Ricerca conferenza non esistente
+  Given l'utente è sulla pagina principale
+    When l'utente seleziona l'opzione "Classifica articoli più citati"
+    And l'utente inserisce "Conferenza non esistente" come titolo della conferenza
+  And l'utente inserisce "2012" come anno
+  And l'utente clicca sul pulsante "Cerca"
+  Then l'utente visualizza il messaggio di errore
+
+  Scenario: Ricerca conferenza con anno non esistente
+    Given l'utente è sulla pagina principale
+    When l'utente seleziona l'opzione "Classifica articoli più citati"
+    And l'utente inserisce "3D Data Processing Visualization and Transmission" come titolo della conferenza
+    And l'utente inserisce "100" come anno
+    And l'utente clicca sul pulsante "Cerca"
+   Then l'utente visualizza il messaggio di errore
--- features/steps/ranking_articles_steps.py
+++ features/steps/ranking_articles_steps.py
@@ -57,7 +57,7 @@ def step_when_click_search(context):
     time.sleep(2)
 
 @step('l\'utente clicca sul menù "Seleziona Classifica"')
-def step_when_user_select_option(context):
+def step_when_user_select_ranking(context):
     button = WebDriverWait(context.driver, 10).until(
         EC.presence_of_element_located((By.ID, 'selectOption'))
     )
@@ -67,14 +67,6 @@ def step_when_user_select_option(context):
     # Seleziona l'opzione con value="classifica"
     dropdown.select_by_value("classifica")
     time.sleep(2)
-#
-# @step('l\'utente clicca sull\'opzione "Classifica"')
-# def step_when_user_select_classifica(context):
-#     button = WebDriverWait(context.driver, 10).until(
-#         EC.element_to_be_clickable((By.XPATH, '//input[@type="option" and @value="Classifica"]'))
-#     )
-#     button.click()
-#     time.sleep(2)
 
 
 @then("l'utente visualizza la classifica degli articoli")
@@ -85,3 +77,54 @@ def step_then_user_sees_article_ranking(context):
     context.driver.quit()
 
 
+@step('l\'utente clicca sul menù "Seleziona Grafico"')
+def step_when_user_select_chart(context):
+    button = WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located((By.ID, 'selectOption'))
+    )
+    button.click()
+    dropdown = Select(button)
+
+    # Seleziona l'opzione con value="grafico"
+    dropdown.select_by_value("grafico")
+    time.sleep(2)
+
+    @then("l'utente visualizza il grafico")
+    def step_then_user_sees_chart(context):
+        # Assicurati di attendere che la pagina si carichi completamente
+        time.sleep(3)
+
+        # Ottieni l'elemento con id "grafico-container"
+        grafico_container = context.driver.find_element(By.ID, "grafico-container")
+
+        # Verifica che l'elemento sia visibile
+        assert grafico_container.is_displayed(), "Il grafico non è visibile"
+
+
+@step('l\'utente inserisce "Conferenza non esistente" come titolo della conferenza')
+def step_when_user_search_conference_not_real(context):
+    input_conference = WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'conference_title'))
+    )
+    input_conference.send_keys("Conferenza non esistente")
+    time.sleep(2)
+
+
+@then("l'utente visualizza il messaggio di errore")
+def step_then_user_sees_error_message(context):
+    # Assicurati di attendere che la pagina si carichi completamente
+    time.sleep(3)
+
+    error_message_container = context.driver.find_element(By.XPATH, "//div[@style='text-align: center; color: red;']")
+
+    # Verifica che l'elemento sia visibile
+    assert error_message_container.is_displayed(), "Il messaggio di errore non è visibile"
+
+
+@step('l\'utente inserisce "100" come anno')
+def step_impl(context):
+    input_conference = WebDriverWait(context.driver, 10).until(
+        EC.presence_of_element_located((By.NAME, 'conference_year'))
+    )
+    input_conference.send_keys("100")
+    time.sleep(2)
